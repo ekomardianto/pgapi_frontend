1aa0b4d5207d04d5a557875d3c9965fbcc2fe068
Eko Mardianto <32917404+ekomardianto@users.noreply.github.com>
2024-11-27 23:08:14 +0700

    clean2
    clean code 2

--- src/components/view/admin/Instansi/ModalUpdateInstansi/index.tsx
+++ src/components/view/admin/Instansi/ModalUpdateInstansi/index.tsx
@@ -1,67 +1,100 @@
-import Button from '@/components/ui/button'
-import Input from '@/components/ui/input'
-import Modal from '@/components/ui/modal'
-import instansiServices from '@/services/instansi'
-import jasaServices from '@/services/jasa'
-import { Instansi } from '@/type/Instansi.type'
-import { Jasa } from '@/type/Jasa.type'
-import React, { Dispatch, FormEvent, SetStateAction, useState } from 'react'
+import Button from "@/components/ui/button";
+import Input from "@/components/ui/input";
+import Modal from "@/components/ui/modal";
+import instansiServices from "@/services/instansi";
+import { Instansi } from "@/type/Instansi.type";
+import React, { Dispatch, FormEvent, SetStateAction, useState } from "react";
 
 type Proptypes = {
-    updateInstansi: Instansi | any
-    setUpdateInstansi: Dispatch<SetStateAction<{}>>
-    setInstansiData: Dispatch<SetStateAction<Instansi[]>>
-    setToaster: Dispatch<SetStateAction<{}>>
-    session: any
-}
+  updateInstansi: Instansi | any;
+  setUpdateInstansi: Dispatch<SetStateAction<{}>>;
+  setInstansiData: Dispatch<SetStateAction<Instansi[]>>;
+  setToaster: Dispatch<SetStateAction<{}>>;
+  session: any;
+};
 const ModalUpdateInstansi = (props: Proptypes) => {
-    const { updateInstansi, setUpdateInstansi, setInstansiData, setToaster, session } = props
+  const {
+    updateInstansi,
+    setUpdateInstansi,
+    setInstansiData,
+    setToaster,
+    session,
+  } = props;
 
-    const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
 
-    const handleUpdateInstansi = async (event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-        setIsLoading(true);
+  const handleUpdateInstansi = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    setIsLoading(true);
 
-        const form: any = event.target as HTMLFormElement;
+    const form: any = event.target as HTMLFormElement;
 
-        const data = {
-            name: form.name.value,
-            alamat: form.alamat.value,
-            phone: form.phone.value
-        }
+    const data = {
+      name: form.name.value,
+      alamat: form.alamat.value,
+      phone: form.phone.value,
+    };
 
-        console.log(data)
-        const result = await instansiServices.updateInstansi(updateInstansi.id, data, session.data?.accessToken);
-        if (result.status === 200) {
-            form.reset();
-            setIsLoading(false);
-            setUpdateInstansi({});
-            setToaster({
-                variant: 'success',
-                message: 'Update Jasa berhasil!'
-            });
-            // feth ulang seluruh data user
-            const { data } = await instansiServices.getAllInstansi(session.data?.accessToken);
-            setInstansiData(data.data);
-        } else {
-            setIsLoading(false);
-        }
+    console.log(data);
+    const result = await instansiServices.updateInstansi(
+      updateInstansi.id,
+      data,
+      session.data?.accessToken
+    );
+    if (result.status === 200) {
+      form.reset();
+      setIsLoading(false);
+      setUpdateInstansi({});
+      setToaster({
+        variant: "success",
+        message: "Update Jasa berhasil!",
+      });
+      // feth ulang seluruh data user
+      const { data } = await instansiServices.getAllInstansi(
+        session.data?.accessToken
+      );
+      setInstansiData(data.data);
+    } else {
+      setIsLoading(false);
     }
+  };
 
-    return (
-        <Modal onClose={() => setUpdateInstansi({})}>
-            <h1>Edit Instansi</h1>
-            <form onSubmit={handleUpdateInstansi}>
-                <Input label='Nama' type='text' name='name' defaultValue={updateInstansi.name} />
-                <Input label='Alamat' type='text' name='alamat' defaultValue={updateInstansi.alamat} />
-                <Input label='Phone/WA' type='number' name='phone' defaultValue={updateInstansi.phone} />
+  return (
+    <Modal onClose={() => setUpdateInstansi({})}>
+      <h1>Edit Instansi</h1>
+      <form onSubmit={handleUpdateInstansi}>
+        <Input
+          label="Nama"
+          type="text"
+          name="name"
+          defaultValue={updateInstansi.name}
+        />
+        <Input
+          label="Alamat"
+          type="text"
+          name="alamat"
+          defaultValue={updateInstansi.alamat}
+        />
+        <Input
+          label="Phone/WA"
+          type="number"
+          name="phone"
+          defaultValue={updateInstansi.phone}
+        />
 
-                <Button type='submit' disabled={isLoading}>{isLoading ? (<div className='box-loader'><div className='loader' /><p>Updating...</p></div>) : 'Update'}</Button>
-            </form>
+        <Button type="submit" disabled={isLoading}>
+          {isLoading ? (
+            <div className="box-loader">
+              <div className="loader" />
+              <p>Updating...</p>
+            </div>
+          ) : (
+            "Update"
+          )}
+        </Button>
+      </form>
+    </Modal>
+  );
+};
 
-        </Modal>
-    )
-}
-
-export default ModalUpdateInstansi
+export default ModalUpdateInstansi;
