187cf98fddbfc0852ec472e6f2b266b6d5530644
Eko Mardianto <32917404+ekomardianto@users.noreply.github.com>
2024-11-28 09:21:54 +0700

    update env di next.config
    update env di next.config dan penambahan clear cache --force

--- next.config.js
+++ next.config.js
@@ -15,6 +15,10 @@ const nextConfig = {
       },
     ],
   },
+  
+  env: {
+    NEXT_PUBLIC_BACKEND_API_BASEURL: process.env.NEXT_PUBLIC_BACKEND_API_BASEURL,
+  },
 };
 
 module.exports = nextConfig;
